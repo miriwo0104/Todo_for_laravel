fff422724e62b754fe92b241342c5044394f1d69
miriwo0517 <shunokawa@gmail.com>
2020-03-27 23:45:47 +0900

    新規TODOを追加するためのリンクをuser_home画面に追加した

--- resources/views/home/user_home.blade.php
+++ resources/views/home/user_home.blade.php
@@ -5,6 +5,9 @@
     
     <h1>usre home</h1>
     <!-- ボタンを追加してTODOを追加できる様にする、新規追加する際の内容はTODO名、優先度、めも -->
+    <a href="/post_todo">
+        <button type="button" class="btn btn-success">新規TODOを追加</button>
+    </a>
     <!-- 大枠のコンテンツ毎にTODOを表示 -->
     <!-- 優先度順に表示する、優先度は三段階くらい、優先度被ったら50音順で表示する -->
 @endsection
