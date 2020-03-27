ea9687d24bcb109742653eeed00265f93678d757
miriwo0517 <shunokawa@gmail.com>
2020-03-27 20:14:07 +0900

    topページに登録画面とログイン画面へのリンクを作成した

--- resources/views/top/index.blade.php
+++ resources/views/top/index.blade.php
@@ -3,5 +3,10 @@
 @section('content')
 
   <p>TODOアプリ</p>
-    
+  <a href="/login">
+    <button type="button" class="btn btn-primary">ログイン</button>
+  </a>
+  <a href="/register">
+    <button type="button" class="btn btn-success">新規登録</button>
+  </a>
 @endsection
